9b938b27718ce2ce5d6a78e8b99dd2100e5d8c75
Edwin Zishiri <ezishiri@censys.io>
2022-06-27 16:17:53 -0400

    docs: CommentsServiceExample

---
+++ packages/censys-search/examples/CommentsServiceExample.ts
@@ -0,0 +1,35 @@
+import { CensysSearch } from "../src";
+
+const client = new CensysSearch({
+    USERNAME: process.env.CENSYS_API_ID,
+    PASSWORD: process.env.CENSYS_API_SECRET,
+});
+
+const IP = "8.8.8.8";
+
+// View a list of comments on the given host
+client.comments.getCommentsByHost(IP).then(console.log).catch(console.error);
+
+// Add a comment on the given host
+client.comments
+    .addCommentByHost(IP, { contents: "This is a test comment" })
+    .then(console.log)
+    .catch(console.error);
+
+// View a specific comment on the given host
+client.comments
+    .getCommentByHost(IP, "959")
+    .then(console.log)
+    .catch(console.error);
+
+// Update a comment on the given host
+client.comments
+    .updateCommentByHost(IP, "959", { contents: "This is an updated comment" })
+    .then(console.log)
+    .catch(console.error);
+
+// Delete a comment on the given host
+client.comments
+    .deleteCommentByHost(IP, "959")
+    .then(console.log)
+    .catch(console.error);
